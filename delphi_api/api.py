d4937d107bfba30a140be1d914401f8e50485459
Akshay <araa@connect.ust.hk>
2020-09-17 18:44:30 +0800

    Add flask+gunicorn+simpleApi+herokuConfigs

--- delphi_api/api.py
+++ delphi_api/api.py
@@ -1,10 +1,31 @@
 import storageClient
 import apiHelper
-import pprint
+from flask import Flask
+from flask import jsonify
 
+
+app = Flask(__name__)
+
+
+# Setup storage client to read storage
 store = storageClient.StorageClient(None)
+# api helper setup
 tools = apiHelper.ApiHelper(store)
+# Set up token prices
+tools.update_token_prices()
 
 
-stats = tools.get_stats()
-pprint.pprint(stats)
+@app.route("/")
+def welcome():
+    return jsonify({"result": "Welcome to delphi api powered by BigQuery and Nansen"})
+
+
+@app.route("/stats")
+def get_stats():
+    try:
+        stats = tools.get_stats()
+        return jsonify(stats)
+    except Exception as e:
+        errormsg = f"Error while grabbing stats: {e}"
+        print(errormsg)
+        return jsonify({"Error": errormsg})
--- delphi_api/apiHelper.py
+++ delphi_api/apiHelper.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 
 class ApiHelper:
@@ -14,12 +15,23 @@ class ApiHelper:
             "0xbed50f08b8e68293bd7db742c4207f2f6e520cd2": "Delphi Aave sUSD",
             "0xeae1a8206f68a7ef629e85fc69e82cfd36e83ba4": "Delphi Curve BUSD",
         }
+        self.price_update_frequency = 60 * 15
+        self.reward_tokens = []
+        self.prices = {}
+        self.last_update = 0
+        self.sleep_seconds = 0.2
+        self.weeks_in_year = 52.1429
 
     def get_usd_price(self, coin_id):
         url = f"{self.url}price?ids={coin_id}&vs_currencies=usd"
-        print(url)
         resp = requests.get(url)
         price = resp.json().get(coin_id, {}).get("usd", None)
+        return price
+
+    def get_usd_price_from_contract(self, address):
+        url = f"{self.url}token_price/ethereum?contract_addresses={address}&vs_currencies=usd"
+        resp = requests.get(url)
+        price = resp.json().get(address, {}).get("usd", None)
         return price
 
     def get_rewards(self):
@@ -29,6 +41,25 @@ class ApiHelper:
         #     print(f"{self.pool_names[pool]}")
         #     print(rewards[pool])
         return rewards
+
+    def update_token_prices(self):
+        rewards = self.get_rewards()
+        self.reward_tokens = []
+        for pool in rewards.keys():
+            for token in rewards[pool]:
+                self.reward_tokens.append(token)
+        self.reward_tokens = list((self.reward_tokens))
+        for token in self.reward_tokens:
+            self.prices[token] = self.get_usd_price_from_contract(token)
+            time.sleep(self.sleep_seconds)
+        self.last_update = time.time()
+
+    # Call this without worrying about rate limits it checks if last update
+    # was long ago, and gets prices accordingly
+    def get_prices(self):
+        if (time.time() - self.last_update) > self.price_update_frequency:
+            self.update_token_prices()
+        return self.prices
 
     def get_apy(self):
         self.storage_client.read_storage()
@@ -54,8 +85,39 @@ class ApiHelper:
         stats = {}
         for pool in apy:
             pool_stats = {}
-            pool_stats["apy"] = apy[pool]
-            pool_stats["rewards"] = rewards[pool]
-            pool_stats["liquidity"] = liquidity[pool]
+            pool_stats["Liquidity"] = liquidity[pool]
+            pool_stats["apy"] = self.calc_apy_breakdown(
+                pool, rewards[pool], apy[pool], liquidity[pool]
+            )
+            pool_stats["Name"] = self.pool_names[pool]
             stats[pool] = pool_stats
         return stats
+
+    def calc_apy_breakdown(self, pool, rewards, apy, liquidity):
+        prices = {}
+        weekly_reward_tokens = list(rewards.keys())
+        for token in weekly_reward_tokens:
+            prices[token] = self.prices[token]
+
+        # now we need to calculate total yearly value of
+        # these tokens
+        apy_stats = {}
+        for token in prices:
+            yearly_amount = rewards[token] * self.weeks_in_year
+            usd_yearly = yearly_amount * prices[token]
+            token_apy = 100 * (usd_yearly) / (liquidity)
+            apy_stats[token] = {
+                "apy": token_apy,
+                "price": prices[token],
+                "yearly_amount": yearly_amount,
+            }
+
+        total_reward_apy = 0
+        for stat in apy_stats:
+            total_reward_apy += apy_stats[stat]["apy"]
+
+        apy_stats["Total"] = total_reward_apy + apy
+
+        apy_stats["CapitalGains"] = apy
+
+        return apy_stats
